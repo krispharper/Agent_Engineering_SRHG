25c77cd0942707184a84f9303375ef837a871cec
Kris Harper <kris.harper@stoneridgeam.com>
2026-02-09 22:13:10 -0600

    Added homework for module 3

--- 03_The_Agent_Loop/aimakerspace/text_utils.py
+++ 03_The_Agent_Loop/aimakerspace/text_utils.py
@@ -1,6 +1,45 @@
 import os
 from typing import List
 
+import pymupdf
+
+
+class PDFFileLoader:
+    def __init__(self, path: str):
+        self.documents = []
+        self.path = path
+
+    def load(self):
+        if os.path.isdir(self.path):
+            self.load_directory()
+        elif os.path.isfile(self.path) and self.path.endswith(".pdf"):
+            self.load_file()
+        else:
+            raise ValueError(
+                "Provided path is neither a valid directory nor a .pdf file."
+            )
+
+    def load_file(self):
+        doc = pymupdf.open(self.path)
+        text = ""
+        for page in doc:
+            text += page.get_text()
+        self.documents.append(text)
+
+    def load_directory(self):
+        for root, _, files in os.walk(self.path):
+            for file in files:
+                if file.endswith(".pdf"):
+                    doc = pymupdf.open(os.path.join(root, file))
+                    text = ""
+                    for page in doc:
+                        text += page.get_text()
+                    self.documents.append(text)
+
+    def load_documents(self):
+        self.load()
+        return self.documents
+
 
 class TextFileLoader:
     def __init__(self, path: str, encoding: str = "utf-8"):
